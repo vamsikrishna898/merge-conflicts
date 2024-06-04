35d5706fdbc5fc4268ebd755ddc349e83f600bfc
Vamsi_krishna <vamsikrishna9071@gmail.com>
2024-06-04 21:46:08 +0530

    commiting merge3

---
+++ Merge3.java
@@ -0,0 +1,10 @@
+class wordbyword
+{
+    public static void main(String[] args)
+    {
+        String str={"TestNG,Maven,BDD,Git,Git-hub"};
+        String[] arrofstr = str.split(",",5);
+        for(String a: arrofstr)
+          System.out.println(a);
+    }
+}
